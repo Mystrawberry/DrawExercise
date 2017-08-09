82633f470754d30d1d8153d7a777083243785691
jimmy <jimmytsangfly@gmail.com>
2017-08-09 16:54:28 +0800

    Initial commit

---
+++ app/src/main/java/com/jimmy/www/drawexercise/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jimmy.www.drawexercise;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
